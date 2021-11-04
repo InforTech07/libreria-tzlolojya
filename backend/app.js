57a28f36ba606daba3d1090d9d85e90b24875c1b
InforTech07 <Techpilot07@outlook.com>
2021-11-03 23:52:48 -0600

    update port

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const {dbConnection} = require('./db/dbConnection.js');
 class Server{
     constructor(){
         this.app = express();
-        this.port = process.env.PORT
+        this.port = 4000
         this.apiUser=`/api/v1/user`;
         this.apiProduct =`/api/v1/product`;
         this.db();
